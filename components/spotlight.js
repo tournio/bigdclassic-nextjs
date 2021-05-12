1d2ce88ec26edecf7548315b99d06571c7f5e771
Scott Stebleton <scott@stebleton.net>
2021-05-12 12:59:36 -0400

    move original text to comment, add text with registration link

--- components/spotlight.js
+++ components/spotlight.js
@@ -5,6 +5,34 @@ import Col from "react-bootstrap/Col";
 import styles from './spotlight.module.scss';
 
 const spotlight = () => {
+  const registrationUrl = "https://www.igbo-reg.com/tournaments/big-d-classic-2021";
+  // const text = (
+  //   <Card.Text>
+  //     We're working hard putting together this year's tournament, in a way that will be both fun and safe for everyone. Keep your eyes peeled!
+  //   </Card.Text>
+  // );
+
+  const text = (
+    <Card.Text>
+      <p>
+        Registration is now open! The deadline is August 4, so what are you waiting for?
+      </p>
+      <a href="https://www.igbo-reg.com/tournaments/big-d-classic-2021"
+         className="btn btn-primary">
+        Register Online
+        {' '}<svg xmlns="http://www.w3.org/2000/svg"
+             width="16"
+             height="16"
+             fill="currentColor"
+             className="bi bi-arrow-right aria-hidden"
+             viewBox="0 0 16 16">
+          <path fill-rule="evenodd"
+                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+        </svg>
+      </a>
+    </Card.Text>
+  )
+
   return (
     <Col xs={12} md={4}>
       <Card>
@@ -14,10 +42,7 @@ const spotlight = () => {
           </Card.Title>
         </Card.Header>
         <Card.Body>
-          <Card.Text>
-            We're working hard putting together this year's tournament, in a way that will be both fun and safe for
-            everyone. Keep your eyes peeled!
-          </Card.Text>
+          {text}
         </Card.Body>
       </Card>
     </Col>
@@ -25,11 +50,3 @@ const spotlight = () => {
 };
 
 export default spotlight;
-
-// //   .col-12.col-md-4
-// //     .card.mb-2
-// //       .card-header.bg-light
-// //         %h4.card-title.mb-0
-// //           Spotlight
-// //       .card-body.pb-4
-// //         {% include 'spotlight.html.twig' %}
