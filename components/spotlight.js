d43d5cec59f5cbab5ea0ecbd92830ea6abeaa5de
Scott Stebleton <scott@stebleton.net>
2021-07-20 05:50:57 -0400

    registration is now closed

--- components/spotlight.js
+++ components/spotlight.js
@@ -12,26 +12,32 @@ const spotlight = () => {
   //   </Card.Text>
   // );
 
+  // const text = (
+  //   <Card.Text>
+  //     <p>
+  //       Registration is now open! The deadline is August 4, so what are you waiting for?
+  //     </p>
+  //     <a href="https://www.igbo-reg.com/tournaments/big-d-classic-2021"
+  //        className="btn btn-primary">
+  //       Register Online
+  //       {' '}<svg xmlns="http://www.w3.org/2000/svg"
+  //            width="16"
+  //            height="16"
+  //            fill="currentColor"
+  //            className="bi bi-arrow-right aria-hidden"
+  //            viewBox="0 0 16 16">
+  //         <path fill-rule="evenodd"
+  //               d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+  //       </svg>
+  //     </a>
+  //   </Card.Text>
+  // )
+
   const text = (
     <Card.Text>
-      <p>
-        Registration is now open! The deadline is August 4, so what are you waiting for?
-      </p>
-      <a href="https://www.igbo-reg.com/tournaments/big-d-classic-2021"
-         className="btn btn-primary">
-        Register Online
-        {' '}<svg xmlns="http://www.w3.org/2000/svg"
-             width="16"
-             height="16"
-             fill="currentColor"
-             className="bi bi-arrow-right aria-hidden"
-             viewBox="0 0 16 16">
-          <path fill-rule="evenodd"
-                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-        </svg>
-      </a>
+      Registration is now closed. We can't wait to see everyone at the tournament!
     </Card.Text>
-  )
+  );
 
   return (
     <Col xs={12} md={4}>
