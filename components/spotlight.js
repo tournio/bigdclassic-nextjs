bd2aae984de9f1e184a5de043f4345ebbb896a14
Scott Stebleton <scott@stebleton.net>
2022-06-15 15:40:28 -0400

    replace svg with icon font

--- components/spotlight.js
+++ components/spotlight.js
@@ -21,15 +21,7 @@ const spotlight = () => {
   //     <a href="https://www.igbo-reg.com/tournaments/big-d-classic-2021"
   //        className="btn btn-primary">
   //       Register Online
-  //       {' '}<svg xmlns="http://www.w3.org/2000/svg"
-  //            width="16"
-  //            height="16"
-  //            fill="currentColor"
-  //            className="bi bi-arrow-right aria-hidden"
-  //            viewBox="0 0 16 16">
-  //         <path fill-rule="evenodd"
-  //               d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-  //       </svg>
+  //       {' '}<i className="bi bi-arrow-right" aria-hidden={true} />
   //     </a>
   //   </Card.Text>
   // )
