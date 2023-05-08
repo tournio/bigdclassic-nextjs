6bc9399e7ec597509b6482a5b82b4a247ad4c709
Scott Stebleton <scott@stebleton.net>
2023-05-08 15:19:05 -0400

    Add Vercel Analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,13 @@
 import '../style/big-d-bs.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../style/index.css'
+import {Analytics} from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics/>
+    </>
+  );
 }
