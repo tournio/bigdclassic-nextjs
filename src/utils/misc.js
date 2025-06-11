3a89542f239b7411aced7889229acb41ebe53974
Scott Stebleton <scott@stebleton.net>
2025-06-11 11:30:08 -0400

    Add hotel details

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -9,3 +9,4 @@ export const useClientReady = () => {
 }
 
 export const REGISTER_URL = 'https://www.tourn.io/tournaments/bigdclassic-2025';
+export const HOTEL_URL = 'https://www.marriott.com/event-reservations/reservation-link.mi?id=1749577100283&key=GRP&app=resvlink';
